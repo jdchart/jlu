74c8a1a5d2f644dbf1324b8406c027143b0de5cd
Jacob Hart <jacob@uc12825.ufralc.mb-per.uhb.fr>
2024-06-11 15:12:13 +0200

    collect files

--- jlu/files/file.py
+++ jlu/files/file.py
@@ -21,3 +21,17 @@ def download_zip(url, path):
     
     # Remove temporary ip file:
     os.remove(temp_zip)
+
+def collect_files(path, acceptedFormats = []):
+    """Collect all files of accepted format in a given directory."""
+
+    finalList = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if len(acceptedFormats > 0):
+                extension = os.path.splitext(file)[1][1:]
+                if extension in acceptedFormats:
+                    finalList.append(os.path.join(root, file))
+            else:
+                finalList.append(os.path.join(root, file))
+    return finalList
